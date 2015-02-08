c0b2b2cf5960979a80c9330882fff7cc4887406f
Michael McManus <michaelorionmcmanus@gmail.com>
2015-02-07 21:56:40 -0800

    Iniial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Prerequisites
+
+* `npm`
+* `bower`
+* `compass`
+
+# Quickstart 
+
+* `npm install && bower install`
+* ` grunt serve`
+
+# Templates
+
+* Home - `/`
+* Article - `/article.html`
+* Issue - `/issue.html`
+* Publication - `publication.html`
+* Search - `search.html`
---
+++ app/scripts/main.js
@@ -0,0 +1,28 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        colorbox: 'colorbox.min'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        },
+        colorbox: {
+            deps: ['jquery']
+        }
+    }
+});
+
+require(['app', 'jquery', 'colorbox'], function (app) {
+    var $ = jQuery;
+    $('a.printSpread').click(function(e) {
+        e.preventDefault();
+        $('a.originalImages').colorbox({
+            rel:'gal',
+            top: '100',
+            transition: "none"
+        });
+        $('a.originalImages:first').click();
+    });
+});
